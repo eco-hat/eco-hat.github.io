2bbdd91ca4daacab9971eda07d00d2bd25c91543
Fudge <fugenkenneth@gmail.com>
2026-05-09 00:19:46 +0800

    Update API base URL to use the correct endpoint for the admin and storage services

--- js/services/Storage.js
+++ js/services/Storage.js
@@ -1,5 +1,5 @@
 export default class Storage {
-    static API_URL = "https://ecohat-fastapi.onrender.com";
+    static API_URL = "https://ecohat-fast-api.vercel.com";
 
     static async register(user) {
         const response = await fetch(`${this.API_URL}/register`, {
